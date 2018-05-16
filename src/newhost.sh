9dea17e620f1a74cc5c760858edcf75ea73b5a0d
Brandon Patterson <brandnpatterson@gmail.com>
2018-05-16 16:21:43 -0500

    Update newhost.sh

--- src/newhost.sh
+++ src/newhost.sh
@@ -14,20 +14,23 @@ fi
 
 if [[ $1 =~ ^c5 ]]; then
     repo_substr=${repo:3}
+elif [[ $1 =~ '_com/'$ ]]; then
+    repo_substr=${repo::${#repo}-4}
 fi
+
 echo 127.0.0.1 $repo_substr.localhost >> /etc/hosts
 
 printf "\n# $repo
 <VirtualHost *:80>
     ServerName $repo_substr.localhost
-    DocumentRoot \"/Users/sg0301656/www/$repo/trunk\"
-    <Directory \"/Users/sg0301656/www/$repo/trunk\">
+    DocumentRoot \"/Users/sg0301656/www/shs/$repo/trunk\"
+    <Directory \"/Users/sg0301656/www/shs/$repo/trunk\">
         Options Indexes FollowSymLinks Includes ExecCGI
         AllowOverride All
         Require all granted
     </Directory>
-    ErrorLog \"logs/$repo.localhost-error_log\"
+    ErrorLog \"logs/$repo_substr.localhost-error_log\"
 </VirtualHost>\n" >> /Applications/XAMPP/xamppfiles/etc/extra/httpd-vhosts.conf
 
 echo New Host Created:
-echo 127.0.0.1 $repo_substr
+echo 127.0.0.1 $repo_substr.localhost
